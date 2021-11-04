5a36f4d7498b835d1702581fe3126f9415a02219
le hai nam <nam281020011@gmail.com>
2021-11-04 21:32:22 +0700

    update theme

--- src/Layouts/app/component/banner.js
+++ src/Layouts/app/component/banner.js
@@ -1,17 +1,161 @@
 const BannerApp = () => {
-    return (
-        <>
-            <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-                <div className="col-md-5 p-lg-5 mx-auto my-5">
-                    <h1 className="display-4 font-weight-normal">Punny headline</h1>
-                    <p className="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-                    <a className="btn btn-outline-secondary" href="#">Coming soon</a>
-                </div>
-                <div className="product-device shadow-sm d-none d-md-block" />
-                <div className="product-device product-device-2 shadow-sm d-none d-md-block" />
+  return (
+    <>
+      <div className="col-md-3 col-sm-4 col-xs-12 blog-left-section">
+        <div className="button-navbar">
+          <input type="text" name="txt" placeholder="Search" />
+        </div>
+        <div className="blog-left-categories blog-common-wide">
+          <h5>Categories</h5>
+          <ul className="list">
+            <li>
+              <a href="#">Catering</a>
+            </li>
+            <li>
+              <a href="#">Community</a>
+            </li>
+            <li>
+              <a href="#">Employment</a>
+            </li>
+            <li>
+              <a href="#">Franchise</a>
+            </li>
+            <li>
+              <a href="#">Kids Corner</a>
+            </li>
+            <li>
+              <a href="#">Our Recipes</a>
+            </li>
+          </ul>
+        </div>
+        <div className="blog-left-filter blog-common-wide ">
+          <h5>Filter</h5>
+          <p>Price: $40 — $350</p>
+          <div className="slider slider-horizontal" id>
+            <div className="slider-track">
+              <div
+                className="slider-track-low"
+                style={{ left: "0px", width: "3.22581%" }}
+              />
+              <div
+                className="slider-selection"
+                style={{ left: "3.22581%", width: "96.7742%" }}
+              />
+              <div
+                className="slider-track-high"
+                style={{ right: "0px", width: "0%" }}
+              />
             </div>
-        </>
-    )
-}
+            <div
+              className="tooltip tooltip-main top"
+              role="presentation"
+              style={{ left: "51.6129%", marginLeft: "-30px" }}
+            >
+              <div className="tooltip-arrow" />
+              <div className="tooltip-inner">50 : 350</div>
+            </div>
+            <div
+              className="tooltip tooltip-min top"
+              role="presentation"
+              style={{
+                left: "3.22581%",
+                marginLeft: "0px",
+                display: "none",
+              }}
+            >
+              <div className="tooltip-arrow" />
+              <div className="tooltip-inner">50</div>
+            </div>
+            <div
+              className="tooltip tooltip-max top"
+              role="presentation"
+              style={{ left: "100%", marginLeft: "0px", display: "none" }}
+            >
+              <div className="tooltip-arrow" />
+              <div className="tooltip-inner">350</div>
+            </div>
+            <div
+              className="slider-handle min-slider-handle round"
+              role="slider"
+              aria-valuemin={40}
+              aria-valuemax={350}
+              aria-valuenow={50}
+              tabIndex={0}
+              style={{ left: "3.22581%" }}
+            />
+            <div
+              className="slider-handle max-slider-handle round"
+              role="slider"
+              aria-valuemin={40}
+              aria-valuemax={350}
+              aria-valuenow={350}
+              tabIndex={0}
+              style={{ left: "100%" }}
+            />
+          </div>
+          <input
+            id="ex2"
+            type="text"
+            className="span2"
+            defaultValue="50,350"
+            data-slider-min={40}
+            data-slider-max={350}
+            data-slider-step={5}
+            data-slider-value="[50,200]"
+            data-value="50,350"
+            style={{ display: "none" }}
+          />
+          <a href="#" className="filter-btn">
+            FILTER
+          </a>
+        </div>
+        <div className="best-deals-product">
+          <h5>BEST DEALS</h5>
+          <div className="best-deal-blog">
+            <div className="best-deal-left">
+              <img src="/img/img20.png" alt="" />
+            </div>
+            <div className="best-deal-right">
+              <p>Lasal Cheese</p>
+              <p>
+                <strong>15.000</strong>
+              </p>
+            </div>
+          </div>
+          <div className="best-deal-blog">
+            <div className="best-deal-left">
+              <img src="/img/img20.png" alt="" />
+            </div>
+            <div className="best-deal-right">
+              <p>Lasal Cheese</p>
+              <p>
+                <strong>15.000</strong>
+              </p>
+            </div>
+          </div>
+          <div className="best-deal-blog">
+            <div className="best-deal-left">
+              <img src="/img/img20.png" alt="" />
+            </div>
+            <div className="best-deal-right">
+              <p>Lasal Cheese</p>
+              <p>
+                <strong>15.000</strong>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="popular-tag blog-common-wide">
+          <h5>Popular Tags</h5>
+          <a href="#">Audio</a>
+          <a href="#">Service</a>
+          <a href="#">Online Order</a>
+          <a href="#">Contact</a>
+          <a href="#">Cupcake</a>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default BannerApp
+export default BannerApp;
--- src/Modules/app/screens/detailProduct/index.js
+++ src/Modules/app/screens/detailProduct/index.js
@@ -1,75 +1,92 @@
-import { formatNumber } from "../../../../Helpers/utils"
+import { formatNumber } from "../../../../Helpers/utils";
 import { useForm } from "react-hook-form";
-import { useCart } from "../../../../hooks/useCart"
+import { useCart } from "../../../../hooks/useCart";
 import { useParams } from "react-router-dom";
 
-
-
 const DetailProductScreenApp = ({ product, cate, type, topping }) => {
-    const { id } = useParams();
-    const { addProduct, cartItems, increase } = useCart();
+  const { id } = useParams();
+  const { addProduct, cartItems, increase } = useCart();
 
-    const isInCart = product => {
-        return !!cartItems.find(item => item.id === product.id);
-    }
+  const isInCart = (product) => {
+    return !!cartItems.find((item) => item.id === product.id);
+  };
 
-    const {register, handleSubmit, formState: {errors}} = useForm();
-    const onSubmit = async (data) => {
-        console.log(data)
-        // addProduct(data)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data);
+    // addProduct(data)
+  };
 
-    };
-
-    return (
-        <div className="container mt-5">
-            
-
-            <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="hidden" defaultValue={product.name} {...register("name")} />
-            <input type="hidden" defaultValue={product.image} {...register("image")} />
-            <input type="hidden" defaultValue={product.description} {...register("description")} />
-            <input type="hidden" defaultValue={product.price} {...register("price")} />
-            <input type="hidden" defaultValue={product.cate_id} {...register("cate_id")} />
-            
-            <div className="row">
-                <div className="col-6">
-                    <img src={product.image} width="400" />
+  return (
+    <>
+      <div className="product-detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-sm-6 col-xs-12">
+              <img className="img-product-detail" src={product.image} alt="" />
+            </div>
+            <div className="col-md-6 col-sm-6 col-xs-12">
+              <h4 className="text-coffee">{product.name}</h4>
+              <div className="star-review-collect">
+                <div className="star-rating">
+                  <i className="fas fa-star" />
+                  <i className="fas fa-star" />
+                  <i className="fas fa-star" />
+                  <i className="fas fa-star" />
+                  <i className="fas fa-star" />
+                  <span
+                    className="star-rating-customer"
+                    style={{ width: "50%" }}
+                  ></span>
                 </div>
-                <div className="col-6">
-                    <h4>{product.name}</h4>
-                    <h5>{formatNumber(product.price)}</h5>
-                    <p>{product.description}</p> 
-                    {/* <p>Category : {cate.name}</p>  */}
-                    {/* <select {...register("type")}>
-                        {type.map((item) => (
-                            <option value={item.name} key={item.id} className="form-control">{item.name}</option>
-                        ))}
-                    </select> */}
-                    
-                    <button type="submit" className="btn btn-primary mt-5">
-                        Create
-                    </button>
-                
+              </div>
+              <p className="desc-detail">
+                If you're planning to start an online store right away, look no
+                further, get this template on templateForest. This product page
+                demonstrates a "Simple Product".
+              </p>
+              <h3 className="text-coffee">{product.price}</h3>
+              <div className="price-textbox">
+                <span className="minus-text">
+                  <i className="fas fa-minus" />
+                </span>
+                <input type="text" name="txt" placeholder={1} pattern="[0-9]" />
+                <span className="plus-text">
+                  <i className="fas fa-plus" />
+                </span>
+              </div>
+              <a href="#" className="btn-large filter-btn">
+                <i className="fa fa-shopping-bag" aria-hidden="true" /> Add to
+                Cart
+              </a>
+              <div className="share-tag">
+                <div className="row">
+                  <div className="col-md-12 col-sm-12 col-xs-12">
+                    <div className="tag-wrap">
+                      <h5>TAGS</h5>
+                      <a href="#" className="tag-btn">
+                        Audio
+                      </a>
+                      <a href="#" className="tag-btn">
+                        Service
+                      </a>
+                      <a href="#" className="tag-btn">
+                        Cupcake
+                      </a>
+                    </div>
+                  </div>
                 </div>
-                {/* <div>
-                    {
-                        isInCart(product) &&
-                        <button
-                            onClick={() => increase(product)}
-                            className="btn btn-outline-primary btn-sm">Add more</button>
-                    }
-
-                    {
-                        !isInCart(product) &&
-                        <button
-                            onClick={() => addProduct(product)}
-                            className="btn btn-primary btn-sm">Add to cart</button>
-                    }
-                </div> */}
-            </div>    
-            </form>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default DetailProductScreenApp
+export default DetailProductScreenApp;

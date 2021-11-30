026e177d99864b1bca87461608fb668ae07442f7
Theanhpham11520 <panh23368@gmail.com>
2021-11-30 09:44:53 +0700

    update status order

--- src/Router/main.js
+++ src/Router/main.js
@@ -3,30 +3,27 @@ import DashboardScreen from "../Modules/auth/screens/dashboard/dash";
 import AppLayout from "../Layouts/app/index";
 import AuthLayout from "../Layouts/auth/index";
 import CategoryListAuth from "../Modules/auth/components/category";
-import CreateFormScreen from "../Modules/auth/screens/category/create";
 import UpdateFormScreen from "../Modules/auth/screens/category/update";
 
 import ListProductComponent from "../Modules/auth/components/product/index";
-import CreateProductScreen from "../Modules/auth/screens/product/create";
 import UpdateProductScreen from "../Modules/auth/screens/product/update";
 
 import ListToppingComponent from "../Modules/auth/components/topping";
-import CreateToppingScreen from "../Modules/auth/screens/topping/create";
 import UpdateToppingScreen from "../Modules/auth/screens/topping/update";
 
 
 import ListOrderComponent from "../Modules/auth/components/order";
-import CreateOrderScreen from "../Modules/auth/screens/order/create";
 import UpdateOrderScreen from "../Modules/auth/screens/order/update";
 
 import ListTypesComponent from "../Modules/auth/components/types";
 import UpdateTypesScreen from "../Modules/auth/screens/types/update";
+
 import BuildingListAuth from "../Modules/auth/components/building";
-import CreateBuildingScreen from "../Modules/auth/screens/building/create";
 import UpdateBuildingScreen from "../Modules/auth/screens/building/update";
-import CreateClassroomScreen from "../Modules/auth/screens/classroom/create";
+
 import UpdateClassroomScreen from "../Modules/auth/screens/classroom/update";
 import ClassroomListAuth from "../Modules/auth/components/classroom";
+
 import HomePageComponentApp from "../Modules/app/components/home";
 import Cart from "../Modules/app/screens/cart";
 import DetailProductComponentApp from "../Modules/app/components/detailProduct";
@@ -62,35 +59,27 @@ const RouteMain = () => {
                 <DashboardScreen exact path="/admin/dashboars" />
 
                 <CategoryListAuth exact path="/admin/categories" />
-                <CreateFormScreen exact path="/admin/categories/create" />
                 <UpdateFormScreen exact path="/admin/categories/:id" />
 
                 <ListProductComponent exact path="/admin/products" />
-                <CreateProductScreen exact path="/admin/products/create" />
                 <UpdateProductScreen exact path="/admin/products/:id" />
 
                 <ListToppingComponent exact path="/admin/toppings" />
-                <CreateToppingScreen exact path="/admin/toppings/create" />
                 <UpdateToppingScreen exact path="/admin/toppings/:id" />
 
                 <ListTypesComponent exact path="/admin/types" />
                 <UpdateTypesScreen exact path="/admin/types/:id" />
 
                 <BuildingListAuth exact path="/admin/building" />
-                <CreateBuildingScreen exact path="/admin/building/create" />
                 <UpdateBuildingScreen exact path="/admin/building/:id" />
 
                 <ClassroomListAuth exact path="/admin/classroom" />
-                <CreateClassroomScreen exact path="/admin/classroom/create" />
                 <UpdateClassroomScreen exact path="/admin/classroom/:id" />
 
                 <ListOrderComponent exact path="/admin/orders" />
-                <CreateOrderScreen exact path="/admin/orders/create" />
                 <UpdateOrderScreen exact path="/admin/orders/:id" />
 
                 <ListUserComponent exact path="/admin/users" />
-                {/* <CreateOrderScreen exact path="/admin/orders/create" />
-                <UpdateOrderScreen exact path="/admin/orders/:id" /> */}
 
               </Switch>
             </AuthLayout>
